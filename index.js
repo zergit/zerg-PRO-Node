b95d432ae39554cf68f130c3f39325ab661990f8
Sergey Igrunin <igrunen@bk.ru>
2022-08-07 14:45:29 +0300

    Проект начало

---
+++ index.js
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+import { Telegraf } from 'telegraf';
+import 'dotenv/config'
+
+// const { PrismaClient } = require('@prisma/client');
+// require('dotenv').config();
+// const { Telegraf } = require('telegraf');
+
+const prisma = new PrismaClient();
+
+class App {
+	async init() {
+		await prisma.$connect();
+		await prisma.user.findFirst({ where: { id: 1 } })
+
+		// console.log(process.env);
+	}
+}
+
+const app = new App();
+app.init();
+
+
+const token = process.env.TOKEN;
+if (!token) {
+	throw new Error('Не задан token');
+}
+
+const bot = new Telegraf(token);
+
+bot.on('text', (ctx) => {
+	ctx.reply('Привет');
+})
+
+bot.launch();
---
+++ prisma/migrations/20220806142449_added_post/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
